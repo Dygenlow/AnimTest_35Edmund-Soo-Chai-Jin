37e04ddbb33a12eefd4eaa105889a717a59963b4
Dygenlow <edmundsoo36@gmail.com>
2020-11-12 14:27:32 +0800

    Update 1.1

--- Assets/Scripts/PlayerController35.cs
+++ Assets/Scripts/PlayerController35.cs
@@ -19,18 +19,15 @@ public class PlayerController35 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W) && isDeath == false)
-        {
-            transform.Translate(Vector3.forward * Time.deltaTime * speed);
-            playerAnim.SetBool("isStrafe", true);
-        }
+        ForwardKey();
 
-        if(Input.GetKeyUp(KeyCode.W) && isDeath == false)
-        {
-            playerAnim.SetBool("isStrafe", false);
-        }
+        BackwardKey();
 
-        if(Input.GetKeyDown(KeyCode.Space) && isDeath == false)
+        LeftKey();
+
+        RightKey();
+
+        if (Input.GetKeyDown(KeyCode.Space) && isDeath == false)
         {
             playerAnim.SetTrigger("trigAttack");
         }
@@ -43,6 +40,66 @@ public class PlayerController35 : MonoBehaviour
             playerAnim.SetTrigger("trigDeath");
 
             isDeath = true;
+        }
+    }
+
+    private void ForwardKey()
+    {
+        if (Input.GetKey(KeyCode.W) && isDeath == false)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            playerAnim.SetBool("isStrafe", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W) && isDeath == false)
+        {
+            playerAnim.SetBool("isStrafe", false);
+        }
+    }
+
+    private void BackwardKey()
+    {
+        if (Input.GetKey(KeyCode.S) && isDeath == false)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            playerAnim.SetBool("isStrafe", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.S) && isDeath == false)
+        {
+            playerAnim.SetBool("isStrafe", false);
+        }
+    }
+
+    private void LeftKey()
+    {
+        if (Input.GetKey(KeyCode.A) && isDeath == false)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.rotation = Quaternion.Euler(0, 270, 0);
+            playerAnim.SetBool("isStrafe", true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A) && isDeath == false)
+        {
+            playerAnim.SetBool("isStrafe", false);
+        }
+    }
+
+    private void RightKey()
+    {
+        if (Input.GetKey(KeyCode.D) && isDeath == false)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+            playerAnim.SetBool("isStrafe", true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D) && isDeath == false)
+        {
+            playerAnim.SetBool("isStrafe", false);
         }
     }
 }
